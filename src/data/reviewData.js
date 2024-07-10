2bc85fe039ad16cbe5bbdd58a205cc4ceeba14b5
miquelven <miquelven.silva@gmail.com>
2024-07-10 15:45:02 -0300

    feat: create reviewdata file

---
+++ src/data/reviewData.js
@@ -0,0 +1,40 @@
+import review1 from "../assets/imgs/review1.png";
+import review2 from "../assets/imgs/review2.png";
+import review3 from "../assets/imgs/review3.png";
+import review4 from "../assets/imgs/review4.png";
+import review5 from "../assets/imgs/review5.avif";
+
+const reviewData = [
+  {
+    id: 1,
+    name: "Pedro Fernandes",
+    text: "Instalações ótimas e equipamentos novos. Atendimento excelente, mas o espaço é pequeno nos horários de pico",
+    src: review4,
+  },
+  {
+    id: 2,
+    name: "Camila Costa",
+    text: "Ambiente limpo, organizado e profissionais qualificados. Aulas coletivas variadas e motivadoras",
+    src: review2,
+  },
+  {
+    id: 3,
+    name: "Rafael Almeida",
+    text: "Ótima academia com equipamentos modernos. Atendimento excelente e bons horários para as aulas",
+    src: review3,
+  },
+  {
+    id: 4,
+    name: "Beatriz Gomes",
+    text: "Adorei a academia! Ambiente acolhedor, equipe prestativa e aulas diversificadas. Super recomendo!",
+    src: review1,
+  },
+  {
+    id: 5,
+    name: "Felipe Santos",
+    text: "Academia bem equipada, ambiente agradável e ótimo atendimento. Recomendo a todos!",
+    src: review5,
+  },
+];
+
+export default reviewData;
