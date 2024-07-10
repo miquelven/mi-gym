3ce057a99578dcb149520f5e69d5b4675c226425
miquelven <miquelven.silva@gmail.com>
2024-07-10 16:21:29 -0300

    feat: create footerData file

---
+++ src/data/footerData.js
@@ -0,0 +1,17 @@
+import footer1 from "../assets/imgs/footer1.png";
+import footer2 from "../assets/imgs/footer2.jpg";
+import footer3 from "../assets/imgs/footer3.png";
+import footer4 from "../assets/imgs/footer4.jpg";
+import footer5 from "../assets/imgs/footer5.png";
+import footer6 from "../assets/imgs/footer6.png";
+
+const footerData = [
+  { id: 1, src: footer1 },
+  { id: 2, src: footer2 },
+  { id: 3, src: footer3 },
+  { id: 4, src: footer4 },
+  { id: 5, src: footer5 },
+  { id: 6, src: footer6 },
+];
+
+export default footerData;
