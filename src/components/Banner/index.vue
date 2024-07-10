519db42f4710f9b8b43d8f9ffefa22f78912b046
miquelven <miquelven.silva@gmail.com>
2024-07-10 09:39:31 -0300

    feat: create banner component

---
+++ src/components/Banner/index.vue
@@ -0,0 +1,42 @@
+<script setup>
+import Container from "../Container/index.vue";
+import bannerImg from "../../assets/imgs/banner.png";
+</script>
+
+<template>
+  <main
+    class="relative z-10 h-[85vh] w-full bg-no-repeat bg-top bg-cover max-sm:h-[70vh]"
+    :style="{ backgroundImage: `url(${bannerImg})` }"
+  >
+    <div class="absolute z-10 inset-0 bg-black/70"></div>
+
+    <Container>
+      <div
+        class="relative z-20 flex h-[85vh] items-center justify-end max-sm:h-[70vh] max-sm:justify-start"
+      >
+        <div class="flex flex-col gap-8">
+          <div class="uppercase flex flex-col gap-2">
+            <h1
+              class="text-[#ef233c] text-7xl font-bold drop-shadow-lg shadow-black max-lg:text-6xl max-sm:text-5xl"
+            >
+              Trabalhe Duro
+            </h1>
+            <h2
+              class="text-5xl font-bold drop-shadow-lg shadow-black max-lg:text-4xl max-sm:text-3xl"
+            >
+              Para obter o sucesso
+            </h2>
+          </div>
+
+          <div>
+            <p
+              class="drop-shadow-lg shadow-black font-light text-xl max-lg:text-base max-sm:text-sm"
+            >
+              Comece pegando inspirações, continue para dar inspirações
+            </p>
+          </div>
+        </div>
+      </div>
+    </Container>
+  </main>
+</template>
