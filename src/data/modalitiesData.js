17109c91c127d177d2ed1664457a2f6876fbb21e
miquelven <miquelven.silva@gmail.com>
2024-07-10 11:21:53 -0300

    feat: create modalitiesdata file

---
+++ src/data/modalitiesData.js
@@ -0,0 +1,33 @@
+import bodyBuildingImg from "../assets/imgs/bodybuilding.png";
+import cardioImg from "../assets/imgs/cardio.webp";
+import fitness from "../assets/imgs/fitness.png";
+import crossfit from "../assets/imgs/crossfit.png";
+
+const modalitiesData = [
+  {
+    id: 1,
+    title: "Body Building",
+    text: "Desenvolva força e definição muscular com nossos treinos intensivos de bodybuilding. Personalize seu programa para alcançar seus objetivos físicos.",
+    src: bodyBuildingImg,
+  },
+  {
+    id: 2,
+    title: "Cardio",
+    text: "Melhore sua resistência cardiovascular com sessões de cardio dinâmicas. Queime calorias e mantenha seu coração saudável com nossos treinos variados.",
+    src: cardioImg,
+  },
+  {
+    id: 3,
+    title: "Fitness",
+    text: "Aprimore seu condicionamento físico geral com nossos treinos de fitness. Envolva-se em atividades diversificadas que combinam força, flexibilidade e resistência.",
+    src: fitness,
+  },
+  {
+    id: 4,
+    title: "Crossfit",
+    text: "Supere seus limites com nossos desafiadores treinos de Crossfit. Desenvolva força, agilidade e resistência com exercícios de alta intensidade.",
+    src: crossfit,
+  },
+];
+
+export default modalitiesData;
