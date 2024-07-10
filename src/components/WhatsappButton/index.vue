c3591717b2698c403f8a389c5d9684282b391be6
miquelven <miquelven.silva@gmail.com>
2024-07-10 11:21:40 -0300

    feat: create whatsapp component

---
+++ src/components/WhatsappButton/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <button
+    class="transition-all duration-500 rounded-md relative z-10 w-44 py-2 text-white bg-[#ef233c] shadow-md shadow-white/10 hover:brightness-75 max-lg:w-40 max-md:w-36"
+  >
+    <v-icon name="bi-whatsapp" class="w-7 h-7 max-lg:w-6 max-lg:h-6" />
+  </button>
+</template>
