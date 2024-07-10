38632f67556e90bd618edc8387ab79e12310970f
miquelven <miquelven.silva@gmail.com>
2024-07-10 12:15:11 -0300

    feat: create sectiontitle component

---
+++ src/components/SectionTitle/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(["textColor"]);
+</script>
+
+<template>
+  <h2
+    class="text-center text-4xl font-bold uppercase"
+    :style="{ color: textColor }"
+  >
+    <slot />
+  </h2>
+</template>
