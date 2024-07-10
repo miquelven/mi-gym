3f6800db5a5043d221c43ebe37b8adce1a84e0a2
miquelven <miquelven.silva@gmail.com>
2024-07-10 15:10:12 -0300

    feat: add plansarea component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Banner from "./components/Banner/index.vue";
 import About from "./components/About/index.vue";
 import Modalities from "./components/Modalities/index.vue";
 import Team from "./components/Team/index.vue";
+import PlansArea from "./components/PlansArea/index.vue";
 </script>
 
 <template>
@@ -12,4 +13,5 @@ import Team from "./components/Team/index.vue";
   <About />
   <Modalities />
   <Team />
+  <PlansArea />
 </template>
