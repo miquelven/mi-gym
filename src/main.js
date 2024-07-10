64ba8374300a2634497165faa69f88e5c68d9785
miquelven <miquelven.silva@gmail.com>
2024-07-10 09:13:03 -0300

    feat: add config of the vue icon

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import "./style.css";
 import "./main.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { FaBars, IoClose } from "oh-vue-icons/icons";
+
+addIcons(FaBars, IoClose);
+
+const app = createApp(App);
+
+app.component("v-icon", OhVueIcon);
+app.mount("#app");
