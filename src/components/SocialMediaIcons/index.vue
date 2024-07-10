2cde52de4ae19e987e4060ed062f3bbb30517956
miquelven <miquelven.silva@gmail.com>
2024-07-10 12:15:24 -0300

    feat: create socialmediaicons component

---
+++ src/components/SocialMediaIcons/index.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div class="flex gap-4">
+    <v-icon
+      name="co-facebook-f"
+      class="transition-all duration-500 w-6 h-6 hover:scale-110 cursor-pointer hover:text-[#ef233c]"
+    />
+    <v-icon
+      name="bi-twitter"
+      class="transition-all duration-500 w-6 h-6 hover:scale-110 cursor-pointer hover:text-[#ef233c]"
+    />
+    <v-icon
+      name="ri-instagram-fill"
+      class="transition-all duration-500 w-6 h-6 hover:scale-110 cursor-pointer hover:text-[#ef233c]"
+    />
+  </div>
+</template>
