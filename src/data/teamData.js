27816e3245c0dfc8850421e0d9a4949bd9f4d87a
miquelven <miquelven.silva@gmail.com>
2024-07-10 12:15:46 -0300

    feat: create teamdata file

---
+++ src/data/teamData.js
@@ -0,0 +1,33 @@
+import bodyBuilder from "../assets/imgs/bodyBuildingTeam.png";
+import cardio from "../assets/imgs/cardioTeam.png";
+import fitness from "../assets/imgs/fitnessTeam.png";
+import crossfit from "../assets/imgs/crossfitTeam.webp";
+
+const teamData = [
+  {
+    id: 1,
+    name: "João Pereira",
+    job: "Treinador Body Builder",
+    src: bodyBuilder,
+  },
+  {
+    id: 2,
+    name: "Mariana Silva",
+    job: "Treinadora Cardio",
+    src: cardio,
+  },
+  {
+    id: 3,
+    name: "Lucas Oliveira",
+    job: "Treinador Fitness",
+    src: fitness,
+  },
+  {
+    id: 4,
+    name: "Ana Souza",
+    job: "Treinadora Crossfit",
+    src: crossfit,
+  },
+];
+
+export default teamData;
