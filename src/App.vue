63b8cbe95094437478e38f30129b0b13ae8739ac
miquelven <miquelven.silva@gmail.com>
2024-07-10 11:21:27 -0300

    feat: add modalities component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Header from "./components/Header/index.vue";
 import Banner from "./components/Banner/index.vue";
 import About from "./components/About/index.vue";
+import Modalities from "./components/Modalities/index.vue";
 </script>
 
 <template>
   <Header />
   <Banner />
   <About />
+  <Modalities />
 </template>
