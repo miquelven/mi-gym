e69506e0d7a37bddca593be03c1d9e648631308a
miquelven <miquelven.silva@gmail.com>
2024-07-10 11:22:07 -0300

    feat: create modalities component

---
+++ src/components/Modalities/index.vue
@@ -0,0 +1,42 @@
+<script setup>
+import WhatsappButton from "../WhatsappButton/index.vue";
+
+import modalitiesData from "../../data/modalitiesData";
+</script>
+
+<template>
+  <section class="my-72">
+    <h2 class="text-center text-4xl font-bold">Modalidades Disponíveis</h2>
+
+    <ul class="grid grid-cols-2 mt-20 max-sm:grid-cols-1">
+      <li
+        v-for="modalities in modalitiesData"
+        :key="modalities.id"
+        class="relative flex flex-col justify-center items-center w-full h-[calc(100vh/2)] max-sm:h-[50vh]"
+      >
+        <div class="absolute bg-black/75 inset-0 z-10"></div>
+        <img
+          :src="modalities.src"
+          alt="Imagem de uma pessoa na academia"
+          class="absolute inset-0 w-full h-full object-cover object-center"
+        />
+
+        <div
+          class="relative z-20 w-full h-full flex flex-col justify-center items-center px-20 gap-8 max-lg:px-5"
+        >
+          <h4
+            class="relative z-20 text-3xl font-bold after:content-[''] after:w-6 after:h-6 after:absolute after:-top-1 after:-left-2 after:bg-[#ef233c] after:z-[-1] max-md:text-2xl"
+          >
+            {{ modalities.title }}
+          </h4>
+          <p
+            class="font-light text-lg text-center max-lg:text-base max-md:text-sm"
+          >
+            {{ modalities.text }}
+          </p>
+          <WhatsappButton />
+        </div>
+      </li>
+    </ul>
+  </section>
+</template>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import "./main.css";
 import App from "./App.vue";
 
 import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { FaBars, IoClose } from "oh-vue-icons/icons";
+import { FaBars, IoClose, BiWhatsapp } from "oh-vue-icons/icons";
 
-addIcons(FaBars, IoClose);
+addIcons(FaBars, IoClose, BiWhatsapp);
 
 const app = createApp(App);
 
