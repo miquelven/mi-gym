f0f073f494324345591cd7b95324975bdf847d8e
miquelven <miquelven.silva@gmail.com>
2024-07-10 15:10:33 -0300

    feat: create plansdata file

---
+++ src/data/plansData.js
@@ -0,0 +1,43 @@
+const plansData = [
+  {
+    id: 1,
+    title: "Padrão",
+    benefits: [
+      "Acesso ao piso do ginásio e equipamento padrão",
+      "Aulas em grupo: ioga, zumbo, pilates",
+      "Consultas fitness gratuitas",
+      "Vestiário e chuveiros",
+      "Grelha nutricional e lanches",
+      "Descontos para membros em mercadorias",
+    ],
+    pricing: "R$90",
+  },
+  {
+    id: 2,
+    title: "Premium",
+    benefits: [
+      "Benefícios do plano padrão incluído",
+      "Reserva prioritária para treinamento pessoal",
+      "Acesso a equipamentos avançados",
+      "Consultas fitness complementares",
+      "eventos e workshops exclusivos para membros",
+      "Descontos em serviços adicionais",
+    ],
+    pricing: "R$135",
+  },
+  {
+    id: 3,
+    title: "Ultimate",
+    benefits: [
+      "Benefícios do planos padrão e premium incluído",
+      "acesso ilimitado a comodidades premium",
+      "Estacionamento reservado ou serviço de manobrista",
+      "Aulas de fitness premium gratuitas",
+      "Planos de treino personalizados",
+      "Acesso prioritário a passes de convidados e eventos",
+    ],
+    pricing: "R$180",
+  },
+];
+
+export default plansData;
